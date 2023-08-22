d28f529aa4c995e9ed111f625b3dd204512c65b2
shccgxqp <shccgxqp@gmail.com>
2023-08-22 13:34:35 +0800

    add resaturanr-list

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+const restaurants = require("./public/jsons/restaurant.json").results;
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect("/restaurants");
+});
+
+app.get("/restaurants", (req, res) => {
+  const keyword = req.query.keyword?.trim();
+  const matchedRestaurants = keyword
+    ? restaurants.filter((rs) =>
+        Object.values(rs).some((property) => {
+          if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase());
+          }
+          return false;
+        })
+      )
+    : restaurants;
+  res.render("index", { restaurants: matchedRestaurants, keyword });
+});
+app.get("/restaurant/:id", (req, res) => {
+  const id = req.params.id;
+  const restaurant = restaurants.find((rs) => rs.id.toString() === id);
+  res.render("detail", { restaurant });
+});
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`);
+});
