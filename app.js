85e6d6039672e969595fcc971a1f6aab190aa2f2
shccgxqp <shccgxqp@gmail.com>
2023-09-15 16:43:37 +0800

    add edit.hbs and put & delete

--- app.js
+++ app.js
@@ -21,22 +21,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
+//ok
 app.get("/", (req, res) => {
   res.redirect("/restaurants");
 });
 
+//ok
 app.get("/restaurants", (req, res) => {
   const keyword = req.query.keyword?.trim();
-  return Restaurant.findAll({ raw: true, where: { name: { [Op.substring]: keyword ? keyword : "" } } }).then((item) => res.render("index", { restaurants: item, keyword }));
+  return Restaurant.findAll({
+    raw: true,
+    where: { name: { [Op.substring]: keyword ? keyword : "" } },
+  }).then((item) => res.render("index", { restaurants: item, keyword }));
 });
 
+//ok
 app.get("/restaurant/:id", (req, res) => {
   const id = req.params.id;
-  return Restaurant.findByPk(id, { raw: true }).then((item) => res.render("detail", { restaurant: item }));
+  return Restaurant.findByPk(id, { raw: true }).then((item) =>
+    res.render("detail", { restaurant: item })
+  );
 });
 
+//ok
 app.get("/restaurant/:id/edit", (req, res) => {
-  return res.send("edit");
+  const id = req.params.id;
+  return Restaurant.findByPk(id, { raw: true }).then((item) =>
+    res.render("edit", { restaurant: item })
+  );
 });
 
 app.get("/restaurant/:id/new", (req, res) => {
@@ -47,12 +59,31 @@ app.post("/restaurant/", (req, res) => {
   return res.send("post id");
 });
 
+//ok
 app.put("/restaurant/:id", (req, res) => {
-  return res.send("put id");
+  const body = req.body;
+  const id = req.params.id;
+
+  return Restaurant.update(
+    {
+      name: body.name,
+      name_en: body.name_en,
+      category: body.category,
+      image: body.image,
+      location: body.location,
+      phone: body.phone,
+      google_map: body.google_map,
+      rating: body.rating,
+      description: body.description,
+    },
+    { where: { id } }
+  ).then(() => res.redirect(`/restaurant/${id}`));
 });
 
+//ok
 app.delete("/restaurant/:id", (req, res) => {
-  return res.send("delete");
+  const id = req.params.id;
+  return Restaurant.destroy({ where: { id } }).then(() => res.redirect("/restaurants"));
 });
 
 app.listen(port, () => {
