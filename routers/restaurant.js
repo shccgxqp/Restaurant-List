2ee44c49751febe4dacf9b208f9c63d016a64522
shccgxqp <shccgxqp@gmail.com>
2023-09-21 15:20:05 +0800

    fix routers

--- routers/restaurant.js
+++ routers/restaurant.js
@@ -69,35 +69,6 @@ router.get('/:id/edit', (req, res, next) => {
   });
 });
 
-router.post('/sort', (req, res, next) => {
-  const sortBy = req.body.sortBy; // 從下拉式選單獲取排序類別
-  console.log(req.body)
-
-  // 在這裡根據排序類別來編寫排序邏輯
-
-
-  return Restaurant.findAll({
-    offset: (page - 1) * limit,
-    limit,
-    raw: true,
-    where: { name: { [Op.substring]: keyword ? keyword : '' } },
-    order: sortCriteria, // 使用排序標準排序數據
-  })
-    .then((item) => {
-      res.render('index', {
-        restaurants: item,
-        keyword,
-        prev: page > 0 ? page - 1 : 0,
-        next: page + 1,
-        page,
-      });
-    })
-    .catch((error) => {
-      error.errorMessage = '資料讀取失敗';
-      next(error);
-    });
-});
-
 router.post('/', (req, res, next) => {
   const body = req.body;
   return Restaurant.create({
