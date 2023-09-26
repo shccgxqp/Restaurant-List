278da4728d1e4be31f4b943f287693cb93dbd843
shccgxqp <shccgxqp@gmail.com>
2023-09-26 13:26:04 +0800

    add user seeders & migrations

---
+++ seeders/20230926050204-initial-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const bcrypt = require('bcryptjs')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const hash = await bcrypt.hash('12345678', 10)
+    await queryInterface.bulkInsert("users", [
+      {
+        name: 'user1',
+        email: 'user1@example.com',
+        password: hash,
+        favorite: '[1, 2, 3]',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'user2',
+        email: 'user2@example.com',
+        password: hash,
+        favorite: '[4, 5, 6]',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null);
+  }
+};
